61129d10a00b821572564935db7074b1f9b3447f
noismik <noismik97@gmail.com>
2024-02-17 16:31:34 +0900

    feat: axios instance

--- src/shared/service/axios/index.ts
+++ src/shared/service/axios/index.ts
@@ -1,1 +1,50 @@
-export {};
+import axios, { AxiosRequestConfig } from 'axios';
+
+const BASE_URL = process.env.REACT_APP_SERVER_URL;
+const instance = axios.create({ baseURL: BASE_URL });
+
+export const getAxiosInstance = (endpoint: string) => {
+  const get = async <T>(queries: object = {}) => {
+    try {
+      const res = await instance.get<T>(endpoint, {
+        params: queries,
+      } as AxiosRequestConfig);
+      const { data, status } = res;
+      return { ...data, status };
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const post = async <T>(params: object = {}, config?: AxiosRequestConfig) => {
+    try {
+      const res = await instance.post<T>(endpoint, params, config);
+      const { data, status } = res;
+      return { ...data, status };
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const patch = async <T>(params: object = {}, config?: AxiosRequestConfig) => {
+    try {
+      const res = await instance.patch<T>(endpoint, params, config);
+      const { data, status } = res;
+      return { ...data, status };
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const remove = async <T>(params: object = {}) => {
+    try {
+      const res = await instance.delete<T>(endpoint, {
+        data: params,
+      });
+      const { data, status } = res;
+      return { ...data, status };
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return { get, post, patch, remove };
+};
+
+export default getAxiosInstance;
