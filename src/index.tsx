e66f8d4920c21e1992cdf2ba45849d976bb6e6e0
noismik <noismik97@gmail.com>
2024-02-17 12:41:18 +0900

    feat: reactquery devtools

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from 'styled-components';
 import './shared/styles/global/normalize.css';
 import './shared/styles/global/global.css';
 import App from './App';
 import theme from './shared/styles/global/theme';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
 
